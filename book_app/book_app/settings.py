0c84b7d01b44bddb51d29b6df052f7b94fb5b095
sana <sanasalhdar@gmail.com>
2023-05-21 22:44:22 +0200

    vercel deploy

--- book_app/book_app/settings.py
+++ book_app/book_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =  'django-insecure-36gq$xeb)tdyho2m6oegz8t*@gvn4*d0tje3q!qn&cjb7^g60
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
    
 
 # Application definition
